da546f308560057da5753f5be3f1d74a8cdd2f6b
Dhanashri Pagar <24383@thoughtworks.com>
2022-07-28 12:57:24 +0530

    Using jsx element with its properties

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const name = "John smith"
+  const jsxElement = <h1>Hello {formatName(name)}!!!</h1> ;
+
+  return jsxElement;
+}
+
+function formatName(name) {
+  return name.toUpperCase()
 }
 
 export default App;
