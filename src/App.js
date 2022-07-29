e63dc1ad1841825f8b92a63f0024a6138ffb45b0
Dhanashri Pagar <24383@thoughtworks.com>
2022-07-29 15:41:47 +0530

    Add userForm component with change handlers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Welcome from './Welcome';
-import Clock from './Clock';
-import ToggleButton from './ToggleButton';
-import Greeting from "./Greeting";
-import LoginControl from "./LoginControl";
-import NumberList from "./NumberList";
+import UserForm from "./UserForm";
 
 function App() {
-    const numbers = [2, 3, 4, 5, "assdad"]
-    const todos = [{id: 1, name: "walking", isDone: true}, {id: 2, name: "coding", isDone: false}]
-    let todoLI = todos.map(todo => <li>{todo}</li>);
     return <div>
-        <NumberList numbers={numbers}/>
-        <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>
-                    {todo.name} : {todo.isDone ? 'DONE' : ''}
-                </li>
-            ))}
-        </ul>
+        <UserForm/>
     </div>
 }
 
---
+++ src/UserForm.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+
+class UserForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {userName: '', description: '', gender: '', acceptTerms: false}
+    }
+
+    render() {
+        return <form onSubmit={this.handleSubmit}>
+            <div>
+                <div>
+                    <label>Name:
+                        <input type="text" name="userName" value={this.state.userName} onChange={this.handleChange}/>
+                    </label>
+                </div>
+                <div>
+                    <label> Gender:
+                        <select value={this.state.gender} onChange={this.handleGenderChange}>
+                            <option>Choose</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                    </label>
+                </div>
+                <div>
+                    <label>Description:
+                        <textarea value={this.state.description} onChange={this.handleDescriptionChange}/>
+                    </label>
+                </div>
+                <div>
+                    <label> Accept Terms:
+                        <input type="checkbox" name="acceptTerms" checked={this.state.acceptTerms}
+                               onChange={this.handleAcceptTermsChange}/>
+                    </label>
+                </div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    }
+
+    handleChange = (event) => {
+        this.setState({userName: event.target.value})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        alert(
+            'Username - ' + this.state.userName +
+            '\nDescription - ' + this.state.description +
+            '\nGender - ' + this.state.gender +
+            '\nAccept terms - ' + this.state.acceptTerms)
+    }
+
+    handleDescriptionChange = (event) => {
+        this.setState({description: event.target.value})
+    }
+
+    handleGenderChange = (event) => {
+        this.setState({gender: event.target.value})
+    }
+
+    handleAcceptTermsChange = (event) => {
+        this.setState({acceptTerms: event.target.checked})
+    }
+}
+
+export default UserForm;
